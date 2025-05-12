6ccf88adbe1d3565d673e486e28912612cac2375
David Gageot <david.gageot@docker.com>
2025-05-12 11:50:12 +0200

    Fix Cloud mode
    Signed-off-by: David Gageot <david.gageot@docker.com>

--- cmd/docker-mcpgateway/config.go
+++ cmd/docker-mcpgateway/config.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"context"
+	"os"
+	"os/exec"
+	"path/filepath"
 
 	"gopkg.in/yaml.v3"
 )
-
-type FileContent struct {
-	VolumeId   string `json:"volumeId"`
-	TargetPath string `json:"targetPath"`
-	Contents   string `json:"contents"`
-}
 
 type Registry struct {
 	Servers map[string]Tile `yaml:"registry"`
@@ -37,10 +34,17 @@ func enabledMCPServers(ctx context.Context) (map[string]Tile, error) {
 }
 
 func readPromptFile(ctx context.Context, name string) (string, error) {
-	var content FileContent
-	if err := get(ctx, httpClient(dialVolumeContents), "/volume-file-content?volumeId=docker-prompts&targetPath="+name, &content); err != nil {
+	home, err := os.UserHomeDir()
+	if err != nil {
 		return "", err
 	}
 
-	return content.Contents, nil
+	// TODO(dga): I wanted to use the volume contents socket but in cloud mode, it isn't talking to the local Docker anymore.
+	path := filepath.Join(home, "Library/Containers/com.docker.docker/Data/docker.raw.sock")
+	out, err := exec.CommandContext(ctx, "docker", "-H", "unix://"+path, "run", "--rm", "-v", "docker-prompts:/docker-prompts", "-w", "/docker-prompts", "busybox", "cat", name).Output()
+	if err != nil {
+		return "", err
+	}
+
+	return string(out), nil
 }
--- cmd/docker-mcpgateway/paths.go
+++ cmd/docker-mcpgateway/paths.go
@@ -7,10 +7,6 @@ import (
 	"path/filepath"
 )
 
-func dialVolumeContents(ctx context.Context) (net.Conn, error) {
-	return dial(ctx, "Library/Containers/com.docker.docker/Data/volume-contents.sock")
-}
-
 func dialJFS(ctx context.Context) (net.Conn, error) {
 	return dial(ctx, "Library/Containers/com.docker.docker/Data/jfs.sock")
 }
