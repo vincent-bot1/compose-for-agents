dfccb027f3b4bcc0a66c65c842530393145f9aeb
David Gageot <david.gageot@docker.com>
2025-05-16 09:47:53 +0200

    Fix the filesystem server on darwin
    Signed-off-by: David Gageot <david.gageot@docker.com>

--- pkg/gateway/start.go
+++ pkg/gateway/start.go
@@ -70,8 +70,9 @@ func (g *Gateway) argsAndEnv(ctx context.Context, serverSpec catalog.Server, ser
 		env = append(env, fmt.Sprintf("%s=%s", name, string(out)))
 	}
 
+	// TODO(dga): handle {{|volume}} and {{|volume-target}} mounts
 	for _, mount := range eval.Expressions(serverSpec.Run.Volumes, serverConfig) {
-		args = append(args, "-v", mount)
+		args = append(args, "-v", mount+":"+mount)
 	}
 
 	return args, env, nil
